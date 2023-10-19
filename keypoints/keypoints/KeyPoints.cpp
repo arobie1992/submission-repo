31806337233e94dd4cc61d8e044970ec7df0d21f
Andrew Robie <arobie1992@yahoo.com>
2023-10-18 23:07:05 -0400

    Bunch of cleanup

--- keypoints/keypoints/KeyPoints.cpp
+++ keypoints/keypoints/KeyPoints.cpp
@@ -23,66 +23,59 @@ class BranchEntry {
     {}
 };
 
+void printBranchEntry(BranchEntry &BE) {
+    errs() << "br_" << BE.id << ": " << BE.file_name << ", " << BE.condition_line << ", " << BE.block_start_line << "\n";    
+}
+
 struct KeyPointsPass : public PassInfoMixin<KeyPointsPass> {
     private: 
     int counter;
     std::set<BasicBlock*> seen;
+    int getStartLine(BasicBlock &BB) {
+        for (auto &I : BB) {
+            return I.getDebugLoc().getLine();
+        }
+        // this shouldn't happen, but shouldn't cause a ton of issues if it does
+        // mostly will probably just be some funky output in the dictionary
+        return -1;
+    };
+    void addBranchTag(Module &M, StringRef file_name, int condition_line, BasicBlock &BB) {
+        if(!seen.insert(&BB).second) {
+            // we've already seen this one and transformed it
+            return;
+        }
+        BranchEntry BE(counter++, file_name, condition_line, getStartLine(BB));
+        printBranchEntry(BE);
+        for (auto &I : BB) {
+            LLVMContext &context = M.getContext();
+            std::vector<Type *> printfArgsTypes({Type::getInt8PtrTy(context)});
+            FunctionType *printfType = FunctionType::get(Type::getInt32Ty(context), printfArgsTypes, true);
+            auto printfFunc = M.getOrInsertFunction("printf", printfType);
+            IRBuilder<> builder(&I);
+            Value *str = builder.CreateGlobalStringPtr("br_" + std::to_string(BE.id) + "\n", "str");
+            std::vector<Value *> argsV({str});
+            builder.CreateCall(printfFunc, argsV, "calltmp");
+            break;
+        }
+    };
     void handleSwitch(StringRef file_name, SwitchInst &SI) {
     };
     void handleBranch(Module &M, StringRef file_name, BranchInst &BI) {
-        // errs() << "I'm a Branch Instruction: " << BI << "\n";
-        
         auto num_ops = BI.getNumOperands();
-        // errs() << "Num ops: " << num_ops << "\n";
-        // if it has under 3 ops, then it likely means that it's a synthetic branch and not one we want to analyze
-        if (num_ops < 3) {
+        // Branches that don't have 3 args seem to be user inserted branches 
+        // things like ifs and loops. Need to check about gotos.
+        if (num_ops != 3) {
             return;
         }
-        auto condition_line = BI.getDebugLoc().getLine();
-        int ctr = 0;
-        // iterate over the branches
-        // branch has first operand 0 as condition
-        // others after that are actual basic block branches
-        // errs() << "Seeing if this works: " << test << "\n";
-        for(auto i = BI.op_begin(); i != BI.op_end(); i++, ctr++) {
-            if (isa<BasicBlock>(*i)) {
-                auto BB = dyn_cast<BasicBlock>(*i);
-                // errs() << "Op " << ctr << " is a basic block\n";
-                // errs() << "BB name: ";
-                // BB->printAsOperand(errs(), false);
-                // errs() << "\n";
-
-                if(seen.insert(BB).second) {
-                    // errs() << "New basic block\n";
-                } else {
-                    // errs() << "Seen this basic block\n";
-                    continue;
-                }
-
-                int start_line;
-                for (auto &I : *BB) {
-                    // get the starting line for the branch dictionary
-                    start_line = I.getDebugLoc().getLine();
-                    break;
-                }
-                BranchEntry BE(counter++, file_name, condition_line, start_line);
-                // errs() << "br_" << BE.id << ": " << BE.file_name << ", " << BE.condition_line << ", " << BE.block_start_line << "\n";
-                for (auto &I : *BB) {
-                    // insert the instruction
-                    LLVMContext &context = M.getContext();
-
-                    // Declare C standard library printf 
-                    std::vector<Type *> printfArgsTypes({Type::getInt8PtrTy(context)});
-                    FunctionType *printfType = FunctionType::get(Type::getInt32Ty(context), printfArgsTypes, true);
-                    auto printfFunc = M.getOrInsertFunction("printf", printfType);
-
-                    IRBuilder<> builder(&I);
-                    Value *str = builder.CreateGlobalStringPtr("br_" + std::to_string(BE.id) + "\n", "str");
-                    std::vector<Value *> argsV({str});
-                    builder.CreateCall(printfFunc, argsV, "calltmp");
-                    break;
-                }
-            }
+        auto condition = BI.getOperand(2);
+        if (isa<BasicBlock>(condition)) {
+            auto BB = dyn_cast<BasicBlock>(condition);
+            addBranchTag(M, file_name,BI.getDebugLoc().getLine(), *BB);
+        }
+        auto alternative = BI.getOperand(1);
+        if (isa<BasicBlock>(alternative)) {
+            auto BB = dyn_cast<BasicBlock>(alternative);
+            addBranchTag(M, file_name,BI.getDebugLoc().getLine(), *BB);
         }
     };
     public:
