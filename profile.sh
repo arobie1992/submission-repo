9b1faaa2ddd1c9426fbad6556e27e00f79ed60be
Andrew Robie <arobie1992@yahoo.com>
2023-10-20 13:03:33 -0400

    Fixing bug in cmake

--- profile.sh
+++ profile.sh
@@ -12,7 +12,7 @@ pushd $(dirname $0)
 # see what Dr. Shen says about this
 # keypoints/buildplugin.sh > /dev/null
 
-clang-17 -g -fpass-plugin=`echo keypoints/build/keypoints/KeyPointsPass.*` "$FILE" keypoints/keypoints/branchlog.c -o "$EXECUTABLE_NAME"
+clang-17 -g -fpass-plugin=`echo keypoints/build/keypoints/KeyPointsPass.*` "$FILE" keypoints/support/branchlog.c -o "$EXECUTABLE_NAME"
 
 instrcnt/countinstrs.sh "$EXECUTABLE_NAME"
 
