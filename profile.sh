db73bf2ef51985e9d5bdeade75a2610d2ac40964
Andrew Robie <arobie1992@yahoo.com>
2023-10-20 21:17:39 -0400

    Using temp dir to make things more flexible

--- profile.sh
+++ profile.sh
@@ -6,14 +6,24 @@ EXECUTABLE_NAME="test-exe"
 
 FILE=$1
 
+pid=$$
+tmpdir="tmp-$pid"
+
 pushd $(dirname $0)
 
-# build the plugin
-# see what Dr. Shen says about this
-# keypoints/buildplugin.sh > /dev/null
+mkdir "$tmpdir"
 
-clang-17 -g -fpass-plugin=`echo keypoints/build/keypoints/KeyPointsPass.*` "$FILE" keypoints/support/branchlog.c -o "$EXECUTABLE_NAME"
+cd "$tmpdir"
 
-instrcnt/countinstrs.sh "$EXECUTABLE_NAME"
+clang-17 -gdwarf-4 -fpass-plugin=`echo ../keypoints/build/keypoints/KeyPointsPass.*` ../"$FILE" ../keypoints/support/branchlog.c -o "$EXECUTABLE_NAME"
+
+../instrcnt/countinstrs.sh "$EXECUTABLE_NAME"
+
+cd ..
+
+cp "$tmpdir"/branch_dictionary.txt ./branch_dictionary.txt."$pid"
+cp "$tmpdir"/branch_trace.txt ./branch_trace.txt."$pid"
+
+rm -rf "$tmpdir"
 
 popd
